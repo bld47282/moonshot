5b59d8fa464f958cd2144e7b9a7120127b2fc43a
Benjamin Davey <bldavey@deakin.edu.au>
2020-11-06 18:10:19 +1100

    Correct syntax errors in Water.lua

--- classes/Water.lua
+++ classes/Water.lua
@@ -5,22 +5,22 @@ function Water:init(y)
     self.minY = y - 20
     self.maxY = y + 20
     self.width = 1280
-    self.heigh = 720
+    self.height = 720
     self.rising = true
 end
 
 function Water:update(dt)
-    if self.rising = true and self.y < self.maxY then
+    if self.rising == true and self.y < self.maxY then
         self.y = self.y + (100 * dt)
-    elseif self.rising = true and self.y >= self.maxY then
+    elseif self.rising == true and self.y >= self.maxY then
         self.rising = false
-    elseif self.rising = false and self.y > self.minY then
+    elseif self.rising == false and self.y > self.minY then
         self.y = self.y - (100 * dt)
-    elseif self.rising = false and self.y <= self.minY then
+    elseif self.rising == false and self.y <= self.minY then
         self.rising = true
     end
 end
 
-function Water:render())
-    love.graphics.rectangle(fill, (width / 2), y, self.width, self.height)
+function Water:render()
+    love.graphics.rectangle('fill', (self.width / 2), self.y, self.width, self.height)
 end
