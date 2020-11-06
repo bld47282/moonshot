37c40bc1051a0230401f00a89d6a55fe27c8f271
Benjamin Davey <bldavey@deakin.edu.au>
2020-11-06 18:04:46 +1100

    Add movement logic to Water class

--- classes/Water.lua
+++ classes/Water.lua
@@ -1,11 +1,26 @@
-GroundSect = Class{}
+Water = Class{}
 
-function GroundSect:init(y)
+function Water:init(y)
     self.y = y
+    self.minY = y - 20
+    self.maxY = y + 20
+    self.width = 1280
+    self.heigh = 720
+    self.rising = true
 end
 
-function GroundSect:update(dt)
+function Water:update(dt)
+    if self.rising = true and self.y < self.maxY then
+        self.y = self.y + (100 * dt)
+    elseif self.rising = true and self.y >= self.maxY then
+        self.rising = false
+    elseif self.rising = false and self.y > self.minY then
+        self.y = self.y - (100 * dt)
+    elseif self.rising = false and self.y <= self.minY then
+        self.rising = true
+    end
 end
 
-function GroundSect:render())
+function Water:render())
+    love.graphics.rectangle(fill, (width / 2), y, self.width, self.height)
 end
