19128d8b79899b774f853a5d0157a50ca44d8839
Benjamin Davey <bldavey@deakin.edu.au>
2020-11-06 18:14:47 +1100

    Make water speed into variable

--- classes/Water.lua
+++ classes/Water.lua
@@ -7,20 +7,21 @@ function Water:init(y)
     self.width = 1280
     self.height = 720
     self.rising = true
+    self.speed = 50
 end
 
 function Water:update(dt)
     if self.rising == true and self.y < self.maxY then
-        self.y = self.y + (100 * dt)
+        self.y = self.y + (self.speed * dt)
     elseif self.rising == true and self.y >= self.maxY then
         self.rising = false
     elseif self.rising == false and self.y > self.minY then
-        self.y = self.y - (100 * dt)
+        self.y = self.y - (self.speed * dt)
     elseif self.rising == false and self.y <= self.minY then
         self.rising = true
     end
 end
 
 function Water:render()
-    love.graphics.rectangle('fill', (self.width / 2), self.y, self.width, self.height)
+    love.graphics.rectangle('fill', 0, self.y, self.width, self.height)
 end
