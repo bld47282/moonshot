8c0e839d8eae78543fb64670be02e1cd32c638ea
Benjamin Davey <bldavey@deakin.edu.au>
2020-11-07 11:10:22 +1100

    Add platform class outline

---
+++ classes/Platform.lua
@@ -0,0 +1,15 @@
+Platform = Class{}
+
+function Platform:init(x, y)
+    self.platformImg = love.graphics.newImage('graphics/platform.png')
+    self.x = x
+    self.y = y
+    self.scale = 1
+end
+
+function Platform:update(dt)
+end
+
+function Platform:render()
+    love.graphics.draw(self.platformImg, self.x, self.y, 0, self.scale, self.scale)
+end
