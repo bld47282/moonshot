e296503c79e16c95f760f9437c5a4153f116a2de
Benjamin Davey <bldavey@deakin.edu.au>
2020-11-12 11:13:27 +1100

    Change game name

--- states/Title.lua
+++ states/Title.lua
@@ -10,7 +10,7 @@ end
 
 function Title:render()
     love.graphics.setFont(bigFont)
-    love.graphics.print('Moonshot', windowWidth / 2, windowHeight / 2)
+    love.graphics.print('Howl', windowWidth / 2, windowHeight / 2)
     love.graphics.print('Press Enter', windowWidth / 2, (windowHeight / 2) + 100)
     love.graphics.setFont(font)
 end
